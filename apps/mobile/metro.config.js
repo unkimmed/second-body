31f02e7bbce04edd2fb3a5eea4166ebfeb8d4371
youngchae <010101chae@gmail.com>
2026-04-15 09:11:08 +0900

    web 실행환경 설정

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -1,7 +1,14 @@
+const path = require('path');
 const { getDefaultConfig } = require('expo/metro-config');
 const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname);
+
+// pnpm 워크스페이스에서 react-dom이 expo-router의 가상 node_modules에 없어서
+// Metro가 찾지 못하는 문제를 직접 경로로 해결
+config.resolver.extraNodeModules = {
+  'react-dom': path.resolve(__dirname, 'node_modules/react-dom'),
+};
 
 // NativeWind의 CSS 처리를 Metro 번들러에 추가
 module.exports = withNativeWind(config, { input: './global.css' });
